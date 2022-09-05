048e17c33e81687577c59651a60f9ada569c30b8
Mars <yh_19316@163.com>
2022-09-06 00:21:21 +0800

    添加梳理笔记

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,49 +1,47 @@
-import { Layout, Menu } from "antd";
-import React from "react";
-import styels from "./index.module.less";
-import { Outlet, Link } from "react-router-dom";
-import { MailOutlined } from "@ant-design/icons";
+import { Layout, Menu } from 'antd'
+import React from 'react'
+import styles from './index.module.less'
+import { Outlet, Link } from 'react-router-dom'
+import { MailOutlined } from '@ant-design/icons'
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Footer, Sider } = Layout
 
 const items = [
   {
-    key: "1",
+    key: '1',
     icon: <MailOutlined />,
     children: [],
     label: <Link to="/manage">投放结案管理</Link>,
   },
   {
-    key: "2",
+    key: '2',
     icon: <MailOutlined />,
     children: [],
     label: <Link to="/resource">投前资源库</Link>,
   },
   {
-    key: "3",
+    key: '3',
     icon: <MailOutlined />,
     children: [],
     label: <Link to="/task">任务中心</Link>,
   },
   {
-    key: "4",
+    key: '4',
     icon: <MailOutlined />,
     children: [],
     label: <Link to="/user">个人中心</Link>,
   },
-];
+]
 
 function LayoutPro() {
   return (
-    <div className={styels.box}>
+    <div className={styles.box}>
       <Layout>
         <Header>header</Header>
         <Content>
           <Layout>
             <Sider>
-              <Menu
-                items={items}
-              />
+              <Menu items={items} />
             </Sider>
             <Content>
               <Outlet />
@@ -53,7 +51,7 @@ function LayoutPro() {
         <Footer>footer</Footer>
       </Layout>
     </div>
-  );
+  )
 }
 
-export default LayoutPro;
+export default LayoutPro
