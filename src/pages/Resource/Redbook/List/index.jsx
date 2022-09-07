8ae66ff8f521007e69cabf75be7c63faa9f58b3c
Mars <yh_19316@163.com>
2022-09-07 22:54:03 +0800

    update: 添加重点注释

--- src/pages/Resource/Redbook/List/index.jsx
+++ src/pages/Resource/Redbook/List/index.jsx
@@ -1,7 +1,8 @@
 // 小红书列表
-import { Tag, Table, Space, Avatar,Divider,Button } from 'antd';
-import React, { useState } from 'react';
-import styles from "./index.module.less"
+import { Tag, Table, Space, Avatar, Divider, Button } from 'antd'
+import React, { useState } from 'react'
+import styles from './index.module.less'
+import IconGift from '../../../../assets/img/icon-gift.svg'
 const columns = [
   {
     title: '达人信息',
@@ -9,31 +10,45 @@ const columns = [
     width: 350,
     ellipsis: true,
     dataIndex: 'info',
-    render: (text,record,index) => {
-      return <Space>
-        <Avatar src={record.headUrl} size={32} />
-        <div className={styles.infoRight}>
-          <div className={styles.baseInfo}>
-            <span>{text}</span>
-            <Tag>LV3</Tag>
-            <img src="" />
-            <img src="" />
+    // render 渲染标签组件 不只是静态组件 里面的参数分别是当前行的值，当前行数据
+    render: (text, record) => {
+      return (
+        <Space>
+          {/* 头像 用组件Avatar 在数据里有一个默认的headUrl头像 */}
+          <Avatar src={record.headUrl} size={32} />
+          {/* 达人信息右边部分 */}
+          <div className={styles.infoRight}>
+            {/* 第一行基本信息 */}
+            <div className={styles.baseInfo}>
+              <span>{text}</span>
+              {/* 标签组件 */}
+              <Tag>LV3</Tag>
+              <img src="" />
+              <img src="" />
+            </div>
+            {/* 第二行地域信息 */}
+            <div className={styles.locationInfo}>
+              <span>四川成都</span>
+              {/* 组件分割线 */}
+              <Divider type="vertical" />
+              <span>
+                <img src="" />
+                大禹机构
+              </span>
+              {/* 组件分割线 */}
+              <Divider type="vertical" />
+              <span>年框签约-新榜</span>
+            </div>
+            {/* 第三行标签信息 */}
+            <div className={styles.tagInfo}>
+              <div>美妆-护肤</div>
+              <div>vlog</div>
+              <div>ootd</div>
+              <div>+3</div>
+            </div>
           </div>
-          <div className={styles.locationInfo}>
-            <span>四川成都</span>
-            <Divider type="vertical" />
-            <span><img src="" />大禹机构</span>
-            <Divider type="vertical" />
-            <span>年框签约-新榜</span>
-          </div>
-          <div className={styles.tagInfo}>
-            <div>美妆-护肤</div>
-            <div>vlog</div>
-            <div>ootd</div>
-            <div>+3</div>
-          </div>
-        </div>
-      </Space>
+        </Space>
+      )
     },
   },
   {
@@ -114,55 +129,62 @@ const columns = [
     fixed: 'right',
     dataIndex: 'address',
   },
-];
+]
 const data = [
-  {address:"123",info:"小明1"},
-  {address:"123",info:"小明2"},
-  {address:"123",info:"小明3"},
-  {address:"123",info:"小明4"},
-  {address:"123",info:"小明5"},
-  {address:"123",info:"小明6"},
-  {address:"123",info:"小明7"},
-  {address:"123",info:"小明8"},
-  {address:"123",info:"小明9"},
-  {address:"123",info:"小明10"},
-]; 
-
+  { address: '123', info: '小明1' },
+  { address: '123', info: '小明2' },
+  { address: '123', info: '小明3' },
+  { address: '123', info: '小明4' },
+  { address: '123', info: '小明5' },
+  { address: '123', info: '小明6' },
+  { address: '123', info: '小明7' },
+  { address: '123', info: '小明8' },
+  { address: '123', info: '小明9' },
+  { address: '123', info: '小明10' },
+]
 
 const List = () => {
-  const [selectedRowKeys,setSelectedRowKeys] = useState([])
+  // 更改复选框的状态
+  const [selectedRowKeys, setSelectedRowKeys] = useState([])
 
   return (
     <div className={styles.tableBox}>
+      {/* 设置水平分割线组件 */}
       <Divider />
       <div className={styles.tableHead}>
         xxxx
-        <Button type="primary" disabled={!selectedRowKeys.length}>批量添加</Button>
+        {/* 设置禁止选中 通过判断条件 当复选框有被选中时 按钮属性为false 不被禁止 */}
+        <Button type="primary" disabled={!selectedRowKeys.length}>
+          批量添加
+        </Button>
       </div>
       <Table
+        // ？
         rowKey="info"
         rowSelection={{
           selectedRowKeys,
-          onChange(keys){
+          onChange(keys) {
             setSelectedRowKeys(keys)
-          }
+          },
         }}
         columns={columns}
         dataSource={data}
+        // 翻页标签配置属性
         pagination={{
-          showQuickJumper:true,
-          showSizeChanger:[10,20,30,60,100],
-          pageSize:20,
-          current:1,
-          total:data.length
+          showQuickJumper: true,
+          showSizeChanger: [10, 20, 30, 60, 100],
+          pageSize: 20,
+          current: 1,
+          total: data.length,
         }}
+        // 列表滚动范围
         scroll={{
           x: 1000,
-          y: 600
+          y: 600,
         }}
       />
     </div>
-  );
-};
+  )
+}
 
-export default List;
+export default List
