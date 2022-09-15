a307681837c355d08e0dae630052f60a58ff5ef8
Mars <yh_19316@163.com>
2022-09-16 00:04:49 +0800

    feat:新增小红书详情页head样式

--- src/pages/ResourceDetail/Redbook/InfoLeft/index.jsx
+++ src/pages/ResourceDetail/Redbook/InfoLeft/index.jsx
@@ -1,80 +1,39 @@
-import React, { useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useContext } from 'react'
 import styles from './index.module.less'
 import IconXHS from './img/icon-official-xiaohongshu.svg'
 // import { Divider } from 'antd'
-import { BTNS_CONFIG } from './sourceData'
+import { BTNS_CONFIG } from '../sourceData'
 import classNames from 'classnames'
 import context from '../context'
 
-const { Provider } = context
-
 const InfoLeft = () => {
-  const { search, pathname } = useLocation()
-  const navigate = useNavigate()
-  // console.log('search:', search)
-  // 当前选中的按钮选项
-  const [checked, setChecked] = useState(getInitChecked())
-  // console.log(checked)
-  // console.log(checked)
-  // 获取初始化的checked选项
-  function getInitChecked() {
-    const paramsList = search.split('&')
-    // console.log(paramsList)
-    const params = paramsList.find((params) => params.includes('type='))
-    // console.log('params', params)
-    if (!params) {
-      return BTNS_CONFIG[0].value
-    } else {
-      const str = params.split('type=').pop()
-      // console.log(str)
-      return str
-    }
-  }
-  // 修改checked的值的方法
-  function dispatch(item) {
-    // 改变当前选中的checked的值
-    setChecked(item.value)
-    // 修改地址栏的值、参数值
-    // 因为会修改复用newSearch变量的值 所以用let
-    let newSearch =
-      search
-        .split('&')
-        .filter((params) => !params.includes('type='))
-        .join('&') + `&type=${item.value}`
-    if (!newSearch.includes('?')) {
-      newSearch = `?${newSearch}`
-    }
-    const href = `${pathname}${newSearch}`
-    navigate(href)
-  }
+  const res = useContext(context)
+  const { dispatch, checked } = res
 
   return (
-    <Provider value={{ checked, dispatch }}>
-      <div className={styles.infoLeft}>
-        <div className={styles.title}>
-          <img src={IconXHS} alt="" />
-          <span>小红书详情</span>
-        </div>
-        <div className={styles.btnGroup}>
-          {BTNS_CONFIG.map((item) => (
-            <div
-              key={item.value}
-              onClick={() => dispatch(item)}
-              className={classNames(
-                styles.btn,
-                checked === item.value && styles.checked
-              )}>
-              <span>{item.label}</span>
-            </div>
-          ))}
-        </div>
-        {/* <Divider>
+    <div className={styles.infoLeft}>
+      <div className={styles.title}>
+        <img src={IconXHS} alt="" />
+        <span>小红书详情</span>
+      </div>
+      <div className={styles.btnGroup}>
+        {BTNS_CONFIG.map((item) => (
+          <div
+            key={item.value}
+            onClick={() => dispatch(item)}
+            className={classNames(
+              styles.btn,
+              checked === item.value && styles.checked
+            )}>
+            <span>{item.label}</span>
+          </div>
+        ))}
+      </div>
+      {/* <Divider>
         <span className={styles.dataInfo}>私有数据</span>
       </Divider>
       <div>历史投放</div> */}
-      </div>
-    </Provider>
+    </div>
   )
 }
 
--- src/pages/ResourceDetail/Redbook/InfoRight/index.jsx
+++ src/pages/ResourceDetail/Redbook/InfoRight/index.jsx
@@ -1,9 +1,14 @@
 import React, { useContext } from 'react'
-import { Button } from 'antd'
+import { Button, Divider } from 'antd'
 import styles from './index.module.less'
 import IconHead from './img/head-photo.jpg'
 import IconGift from '@/assets/img/icon-gift.svg'
 import IconShopCart from '@/assets/img/icon-shopcart.svg'
+import IconSexual from '@/assets/img/icon-female.svg'
+import IconMCN from '@/assets/img/icon-mcn.svg'
+import IconOperate from './img/in-cooperate.png'
+import IconPugongying from './img/pugongying.png'
+import IconPugongying_BG from './img/pugongying_bg.png'
 import context from '../context'
 import SprPerfor from './components/SprPerfor'
 import PriPerfor from './components/PriPerfor'
@@ -11,36 +16,101 @@ import PriPerfor from './components/PriPerfor'
 const InfoRight = () => {
   // ?
   const { checked } = useContext(context)
-  console.log(checked)
+  // console.log(checked)
   return (
     <div className={styles.infoRight}>
       <div className={styles.head}>
-        <div className={styles.titleLeft}>
-          <img src={IconHead} alt="" />
-          <div className={styles.button}>
-            <div className={styles.checkBtn}>
-              <Button>查看主页</Button>
-              <Button>更新数据</Button>
+        <div className={styles.head_content}>
+          <div className={styles.headLeft}>
+            <div className={styles.infoBox}>
+              <img src={IconHead} alt="" />
+              <img className={styles.sex} src={IconSexual} alt="" />
+              <div className={styles.button}>
+                <div className={styles.checkBtn}>
+                  <Button>查看主页</Button>
+                  <Button>更新数据</Button>
+                </div>
+                <Button>+ 添加</Button>
+              </div>
             </div>
-            <Button>+ 添加</Button>
+            <div className={styles.dataBox}>
+              <div className={styles.baseInfo}>
+                <span className={styles.name}>赵露思</span>
+                <span className={styles.level}>
+                  <span>LV3</span>
+                </span>
+                <div className={styles.firstImg}>
+                  <img src={IconGift} alt="" />
+                </div>
+                <div>
+                  <img src={IconShopCart} alt="" />
+                </div>
+              </div>
+              <div className={styles.tagInfo}>
+                <div className={styles.cursorHover}>美妆-护肤教程</div>
+                <div className={styles.cursorHover}>颜值</div>
+                <div className={styles.cursorHover}>美少女</div>
+              </div>
+              <div className={styles.locationInfo}>
+                <span className={styles.idInfo}>
+                  <span>小红书号</span>
+                  <span>125844661455</span>
+                </span>
+                <Divider type="vertical" />
+                <span>四川成都</span>
+                <Divider type="vertical" />
+                <span className={styles.company}>
+                  <img src={IconMCN} alt="" />
+                  <span>大禹机构</span>
+                </span>
+                <Divider type="vertical" />
+                <span>年框签约机构-新榜</span>
+              </div>
+              <div className={styles.introduction}>
+                <span>简介</span>
+                <span
+                  title="各平台同名 🍑小店主 ✨美妆 | 护肤 | 生活
+                爱凤小百科在线等你～📮shian 爱凤小百科在线等你，
+                爱凤小百科在线等你～⭐️合作：xcz201901(直播) ...">
+                  各平台同名 🍑小店主 ✨美妆 | 护肤 | 生活
+                  爱凤小百科在线等你～📮shian 爱凤小百科在线等你，
+                  爱凤小百科在线等你～⭐️合作：xcz201901(直播) ...
+                </span>
+              </div>
+              <div className={styles.dataInfo}>
+                <div className={styles.fansCount}>
+                  <span>粉丝数</span>
+                  <span>50.4w</span>
+                </div>
+                <div className={styles.likesCount}>
+                  <span>赞藏数</span>
+                  <span>173.72w</span>
+                </div>
+                <div className={styles.notesCount}>
+                  <span>笔记数</span>
+                  <span>350</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.headRight}>
+            <div className={styles.right_title}>
+              <span>数据来源:</span>
+              <img src={IconPugongying} alt="" />
+              <span>蒲公英</span>
+            </div>
+            <div className={styles.bg}>
+              <img src={IconPugongying_BG} alt="" />
+            </div>
           </div>
         </div>
-        <div className={styles.titleRight}>
-          <div className={styles.dataContainer}>
-            <div className={styles.infoList}>
-              <span className={styles.name}>丁郑美子</span>
-              <span>LV4</span>
-              <div>
-                <img src={IconGift} alt="" />
-              </div>
-              <div>
-                <img src={IconShopCart} alt="" />
-              </div>
-            </div>
-            <div></div>
-            <div></div>
-          </div>
-          <div className={styles.dataList}></div>
+        <Divider dashed className={styles.divider} />
+        <div className={styles.tagBox}>
+          <img src={IconOperate} alt="" />
+          <div className={styles.cursorHover}>内部资源</div>
+          <div className={styles.cursorHover}>历史合作</div>
+          <div className={styles.cursorHover}>年框签约</div>
+          <div className={styles.cursorHover}>合作品牌数·10个</div>
         </div>
       </div>
       <div className={styles.content}>
