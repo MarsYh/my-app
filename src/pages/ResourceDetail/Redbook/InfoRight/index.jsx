30ac6c3421ec8ebd3751cedb356558befc12a4f5
Mars <yh_19316@163.com>
2022-09-16 18:24:54 +0800

    feat:新增详情页细节

--- src/pages/ResourceDetail/Redbook/InfoRight/index.jsx
+++ src/pages/ResourceDetail/Redbook/InfoRight/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Button, Divider } from 'antd'
+import { Button, Divider, Input } from 'antd'
 import styles from './index.module.less'
 import IconHead from './img/head-photo.jpg'
 import IconGift from '@/assets/img/icon-gift.svg'
@@ -9,10 +9,14 @@ import IconMCN from '@/assets/img/icon-mcn.svg'
 import IconOperate from './img/in-cooperate.png'
 import IconPugongying from './img/pugongying.png'
 import IconPugongying_BG from './img/pugongying_bg.png'
+import IconOfficialPrice from './img/icon-official-price.svg'
+import IconPrivatePrice from './img/icon-private-price.svg'
+import { QuestionCircleOutlined } from '@ant-design/icons'
 import context from '../context'
 import SprPerfor from './components/SprPerfor'
 import PriPerfor from './components/PriPerfor'
 
+const { TextArea } = Input
 const InfoRight = () => {
   // ?
   const { checked } = useContext(context)
@@ -114,7 +118,67 @@ const InfoRight = () => {
         </div>
       </div>
       <div className={styles.content}>
-        <div className={styles.contentLeft}>left</div>
+        <div className={styles.contentLeft}>
+          <div className={styles.official_price}>
+            <div>
+              <img src={IconOfficialPrice} alt="" />
+            </div>
+            <div className={styles.picture_price}>
+              <span className={styles.price}>图文笔记一口价</span>
+              <span className={styles.number}>¥17000</span>
+            </div>
+            <div className={styles.video_price}>
+              <span className={styles.price}>视频笔记一口价</span>
+              <span className={styles.number}>¥27000</span>
+            </div>
+            <div className={styles.live_normal_price}>
+              <span className={styles.price}>直播拼场报价</span>
+              <span className={styles.number}>¥37000</span>
+            </div>
+            <div className={styles.live_special_price}>
+              <span className={styles.price}>直播专场报价</span>
+              <span className={styles.number}>¥37000</span>
+            </div>
+            <div className={styles.dataTime}>
+              <div>数据更新时间：</div>
+              <div>2022/09/16 12:00:00</div>
+            </div>
+          </div>
+          <div className={styles.private_price}>
+            <div className={styles.privateHead}>
+              <img src={IconPrivatePrice} alt="" />
+              <div className={styles.note}>
+                <QuestionCircleOutlined />
+                <span>说明</span>
+              </div>
+            </div>
+            <div className={styles.picture_price_input}>
+              <div>图文笔记一口价</div>
+              <Input placeholder="请填写实际报价" />
+            </div>
+            <div className={styles.video_price_input}>
+              <div>视频笔记一口价</div>
+              <Input placeholder="请填写实际报价" />
+            </div>
+            <div className={styles.live_normal_price_input}>
+              <div>直播拼场报价</div>
+              <Input placeholder="请填写实际报价" />
+            </div>
+            <div className={styles.live_special_price_input}>
+              <div>直播专场报价</div>
+              <Input placeholder="请填写实际报价" />
+            </div>
+            <div className={styles.note_input}>
+              <div>报价备注</div>
+              <TextArea
+                rows={4}
+                placeholder="请填写其他类型的报价
+备注"
+                maxLength={6}
+              />
+            </div>
+          </div>
+        </div>
         <div className={styles.contentRight}>
           {checked === 'spread_performance' && <SprPerfor />}
           {checked === 'price_performance' && <PriPerfor />}
