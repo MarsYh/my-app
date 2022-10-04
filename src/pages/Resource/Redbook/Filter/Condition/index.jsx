281ab6dd60a7267977f94d23179e23588bf2f5fd
Mars <yh_19316@163.com>
2022-10-04 21:11:44 +0800

    feat:新增筛选样式

--- src/pages/Resource/Redbook/Filter/Condition/index.jsx
+++ src/pages/Resource/Redbook/Filter/Condition/index.jsx
@@ -1,5 +1,7 @@
 // 当前的筛选条件
 import React from 'react'
+import { Tag } from 'antd'
+import { UndoOutlined } from '@ant-design/icons'
 import FilterRow from '../../../components/FilterRow'
 import styles from './index.module.less'
 
@@ -11,12 +13,15 @@ const Condition = (props) => {
   return (
     <FilterRow title="已选条件">
       {/* 当前的筛选内容 */}
-      <div>
+      <div className={styles.filter}>
         {keys.map((key) => (
-          <div key={key}>{selectedRecord[key]}</div>
+          <div className={styles.tags}>{selectedRecord[key]}</div>
         ))}
+        <div className={styles.clearBtn}>
+          <UndoOutlined />
+          重置所有筛选
+        </div>
       </div>
-      <div className={styles.clearBtn}>重置所有筛选</div>
     </FilterRow>
   )
 }
--- src/pages/Resource/Redbook/Filter/ContentTag/index.jsx
+++ src/pages/Resource/Redbook/Filter/ContentTag/index.jsx
@@ -1,160 +1,162 @@
 // 内容标签
-import React from "react";
-import { Checkbox, Popover, Tag, Typography } from "antd";
-import styles from "./index.module.less";
-import FilterRow from "../../../components/FilterRow";
-import { useXhsResource } from "@/store/xhsResource";
-import { useState, useEffect, useRef } from "react";
-import { useDebounce, useSize, useDebounceFn } from "ahooks";
-import FilterSelected from "../../../components/FilterSelected";
-import classNames from "classnames";
+import React from 'react'
+import { Checkbox, Popover, Tag, Typography } from 'antd'
+import styles from './index.module.less'
+import FilterRow from '../../../components/FilterRow'
+import { useXhsResource } from '@/store/xhsResource'
+import { useState, useEffect, useRef } from 'react'
+import { useDebounce, useSize, useDebounceFn } from 'ahooks'
+import FilterSelected from '../../../components/FilterSelected'
+import classNames from 'classnames'
 
 function ContentTag(props) {
-  const { dataSource, selectedRecord, setSelectedRecord } = props;
-  const { contentTags: options = [] } = dataSource;
+  const { dataSource, selectedRecord, setSelectedRecord } = props
+  const { contentTags: options = [] } = dataSource
 
-  const ref = useRef(null);
-  const { CheckableTag } = Tag;
-  const { tableParams, dispatch } = useXhsResource();
-  const [contentTags, setContentTags] = useState([]);
+  const ref = useRef(null)
+  const { CheckableTag } = Tag
+  const { tableParams, dispatch } = useXhsResource()
+  const [contentTags, setContentTags] = useState([])
   // 是否被折叠
-  const [collapse, setCollapse] = useState(true);
-  /**
-   * 防抖：
-   *
-   * 节流：
-   */
-
+  const [collapse, setCollapse] = useState(true)
   // 展开/收起 是否显示
-  const [isShow, setIsShow] = useState(false);
-  const size = useSize(ref) || { size: {} };
-  const debouncedValue = useDebounce(size.width, { wait: 500 });
+  const [isShow, setIsShow] = useState(false)
+  const size = useSize(ref) || { size: {} }
+  const debouncedValue = useDebounce(size.width, { wait: 500 })
 
   // 设置防抖函数
   const { run } = useDebounceFn(
     (contentTags) => {
-      const _tableParams = { ...tableParams };
+      const _tableParams = { ...tableParams }
       if (contentTags.length) {
-        _tableParams.contentTags = contentTags;
+        _tableParams.contentTags = contentTags
       } else {
-        delete _tableParams.contentTags;
+        delete _tableParams.contentTags
       }
-      dispatch(_tableParams);
+      dispatch(_tableParams)
       // 渲染筛选条件
-      renderSelected(contentTags);
+      renderSelected(contentTags)
     },
     {
-      wait: 1000,
+      wait: 500,
     }
-  );
+  )
 
   function del(item, level2Child) {
     // 1. 删除条件
-    // 2. 改变表格参数
-    let _contentTags = [...contentTags];
+    let _contentTags = [...contentTags]
     if (level2Child) {
-      const idx = _contentTags.findIndex((o) => o.level1 === item.level1);
-      const current = _contentTags[idx];
-      current.level2 = current.level2.filter((str) => str !== level2Child);
+      const idx = _contentTags.findIndex((o) => o.level1 === item.level1)
+      const current = _contentTags[idx]
+      current.level2 = current.level2.filter((str) => str !== level2Child)
       // 判断是否有剩余
       if (!current.level2.length) {
-        _contentTags.splice(idx, 1);
+        _contentTags.splice(idx, 1)
       }
     } else {
-      _contentTags = _contentTags.filter((o) => o.level1 !== item.level1);
+      _contentTags = _contentTags.filter((o) => o.level1 !== item.level1)
     }
-    renderSelected(_contentTags);
-    setContentTags(_contentTags);
-    // 改变表格参数
-    run(_contentTags);
+    renderSelected(_contentTags)
+    setContentTags(_contentTags)
+    // 2.改变表格参数
+    run(_contentTags)
   }
 
   // 渲染条件
   function renderSelected(contentTags) {
-    const record = { ...selectedRecord };
+    const record = { ...selectedRecord }
     record.contentTags = contentTags.map((selectedData) => {
-      const { level1, level2 } = selectedData;
-      const original = options.find((o) => o.level1 === level1);
+      const { level1, level2 } = selectedData
+      const original = options.find((o) => o.level1 === level1)
 
-      // 没有子项的情况下
+      // 没有子项的情况
       if (!level2.length) {
         return (
           <FilterSelected key={level1} onDel={() => del(selectedData)}>
-            <div>内容标签:{level1}</div>
+            <div>
+              内容标签:
+              <span className={styles.tagContent}>{level1}</span>
+            </div>
           </FilterSelected>
-        );
+        )
       }
 
       // 子项被全选的情况
       if (original.level2?.length === level2?.length) {
         return (
           <FilterSelected key={level1} onDel={() => del(selectedData)}>
-            <div>内容标签:{level1}-全部</div>
+            <div>
+              内容标签:
+              <span className={styles.tagContent}>{level1}-全部</span>
+            </div>
           </FilterSelected>
-        );
+        )
       }
       // 子项被部分选择的情况
       return level2?.map((level) => (
         <FilterSelected key={level} onDel={() => del(selectedData, level)}>
           <div>
-            内容标签:{level1}-{level}
+            内容标签:
+            <span className={styles.tagContent}>
+              {level1}-{level}
+            </span>
           </div>
         </FilterSelected>
-      ));
-    });
-    setSelectedRecord(record);
+      ))
+    })
+    setSelectedRecord(record)
   }
 
-  /** 展开或者收起功能 */
+  // 展开或者收起功能
   useEffect(() => {
     if (debouncedValue) {
-      setIsShow(ref?.current?.clientHeight > 20);
+      setIsShow(ref?.current?.clientHeight > 20)
     }
-  }, [debouncedValue, ref]);
+  }, [debouncedValue, ref])
 
   // 一级标签筛选
   function onFirstTagChange(item, checked) {
-    const _contentTags = [...contentTags];
+    const _contentTags = [...contentTags]
     // 被选中
     if (checked) {
-      const o = { level1: item.level1, level2: item.level2 };
-      _contentTags.push(o);
+      const o = { level1: item.level1, level2: item.level2 }
+      _contentTags.push(o)
     } else {
-      // 取消
-      const idx = _contentTags.findIndex((o) => o.level1 === item.level1);
-      _contentTags.splice(idx, 1);
+      // 取消选中
+      const idx = _contentTags.findIndex((o) => o.level1 === item.level1)
+      _contentTags.splice(idx, 1)
     }
-    setContentTags(_contentTags);
+    setContentTags(_contentTags)
     // 请求表格数据
-    run(_contentTags);
+    run(_contentTags)
   }
 
   // 二级标签筛选
   function onSecondTagChange(item, checkedList) {
-    const _contentTags = [...contentTags];
-    const idx = _contentTags.findIndex((o) => o.level1 === item.level1);
+    const _contentTags = [...contentTags]
+    const idx = _contentTags.findIndex((o) => o.level1 === item.level1)
     if (checkedList.length) {
-      const o = { level1: item.level1, level2: checkedList };
+      const o = { level1: item.level1, level2: checkedList }
       if (idx === -1) {
-        _contentTags.push(o);
+        _contentTags.push(o)
       } else {
-        _contentTags[idx] = o;
+        _contentTags[idx] = o
       }
     } else {
-      _contentTags.splice(idx, 1);
+      _contentTags.splice(idx, 1)
     }
-    setContentTags(_contentTags);
+    setContentTags(_contentTags)
     // 请求表格数据
-    run(_contentTags);
+    run(_contentTags)
   }
 
   // 获取被选中的子项
   function getCheckedList(item) {
-    return contentTags.find((o) => o.level1 === item.level1)?.level2;
+    return contentTags.find((o) => o.level1 === item.level1)?.level2
   }
 
   function handleCollapse() {
-    setCollapse(!collapse);
+    setCollapse(!collapse)
   }
 
   return (
@@ -172,23 +174,23 @@ function ContentTag(props) {
                     onChange={(checkedList) =>
                       onSecondTagChange(item, checkedList)
                     }
-                    value={getCheckedList(item)}
-                  >
+                    value={getCheckedList(item)}>
                     {item.level2?.map((str) => (
-                      <Checkbox key={str} value={str}>
+                      <Checkbox
+                        className={styles.checkColor}
+                        key={str}
+                        value={str}>
                         {str}
                       </Checkbox>
                     ))}
                   </Checkbox.Group>
-                }
-              >
+                }>
                 <CheckableTag
                   checked={contentTags.some(
                     (tag) => item.level1 === tag.level1
                   )}
                   className={styles.tag}
-                  onChange={(checked) => onFirstTagChange(item, checked)}
-                >
+                  onChange={(checked) => onFirstTagChange(item, checked)}>
                   {item.level1}
                 </CheckableTag>
               </Popover>
@@ -196,24 +198,22 @@ function ContentTag(props) {
               <CheckableTag
                 checked={contentTags.some((tag) => item.level1 === tag.level1)}
                 className={styles.tag}
-                onChange={(checked) => onFirstTagChange(item, checked)}
-              >
+                onChange={(checked) => onFirstTagChange(item, checked)}>
                 {item.level1}
               </CheckableTag>
             )
           )}
-          {/* 逻辑控制 */}
+          {/* 逻辑控制展开或收起 */}
           {isShow && (
             <Typography.Link
               className={styles.collapse}
-              onClick={handleCollapse}
-            >
-              {collapse ? "展开" : "收起"}
+              onClick={handleCollapse}>
+              {collapse ? '展开' : '收起'}
             </Typography.Link>
           )}
         </div>
       </div>
     </FilterRow>
-  );
+  )
 }
-export default ContentTag;
+export default ContentTag
